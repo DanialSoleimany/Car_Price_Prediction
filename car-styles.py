34a169db3d8083155b11aebaa49dd16043d4cf00
Danial Soleimany <102956315+DanialSoleimany@users.noreply.github.com>
2025-01-11 21:17:04 +0330

    Add files via upload

---
+++ car-styles.py
@@ -0,0 +1,29 @@
+# Define reusable styles
+style_block = """
+<style>
+.styled-div {
+    background: #D473D4; 
+    color: #ffffff; 
+    border-radius: 50px; 
+    padding: 10px 15px; 
+    font-size: 18px; 
+    font-weight: bold; 
+    text-align: center; 
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; 
+    margin-bottom: 20px;
+}
+
+.nav-link {
+    display: inline-block; 
+    background-color: #9b4dfa; 
+    color: white; 
+    padding: 5px 10px; 
+    text-align: center; 
+    text-decoration: none; 
+    border-radius: 5px; 
+    font-family: 'Arial', sans-serif; 
+    font-size: 10px; 
+    float: left;
+}
+</style>
+"""
